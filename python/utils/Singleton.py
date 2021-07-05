22b755f15a6449f56756693bdec7bedfce9db4e7
Sebastien Cossin <sebastien.cossin@chu-bordeaux.fr>
2021-07-05 16:56:42 +0200

    add Singleton class

---
+++ python/utils/Singleton.py
@@ -0,0 +1,11 @@
+from typing import Dict
+
+
+class Singleton(type):
+    _instances: Dict[type, type] = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
